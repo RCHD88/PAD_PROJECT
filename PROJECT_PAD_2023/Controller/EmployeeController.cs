295d6d25d484ee9d8ec18f9c703e364f3dbb2fd4
RCHD88 <richardraferguy@gmail.com>
2023-12-04 22:38:26 +0700

    login employee

---
+++ PROJECT_PAD_2023/Controller/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_PAD_2023.Controller
+{
+    internal class EmployeeController
+    {
+        AdventureWorks2022Entities database;
+        public EmployeeController()
+        {
+            database = new AdventureWorks2022Entities();
+        }
+        public Employee loginAuthentication(string email)
+        {
+            try
+            {
+                Employee employee = database.EmailAddresses.Where(em => em.EmailAddress1.Equals(email)).FirstOrDefault().Person.Employee;
+                return employee;
+            }
+            catch (NullReferenceException ex)
+            {
+                return null;
+            }
+        }
+    }
+}
--- PROJECT_PAD_2023/Main.cs
+++ PROJECT_PAD_2023/Main.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using PROJECT_PAD_2023.Controller;
 
 namespace PROJECT_PAD_2023
 {
     public partial class Main : Form
     {
+        EmployeeController employeeController;
+        Employee employee;
         public Main()
         {
             InitializeComponent();
+            employeeController = new EmployeeController(); 
+        }
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string email = emailTextBox.Text;
+            employee =  employeeController.loginAuthentication(email);
+            if(employee != null )
+            {
+                MessageBox.Show("Berhasil login");
+            }
+            else
+            {
+                MessageBox.Show("Employee tersebut tidak terdaftar");
+            }
         }
     }
 }
