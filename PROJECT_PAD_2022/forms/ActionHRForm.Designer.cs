afcd8427140f1fd820b1e16cd11adf938ada1c5d
RCHD88 <richardraferguy@gmail.com>
2023-12-09 01:34:21 +0700

    recommit

--- PROJECT_PAD_2022/forms/ActionHRForm.Designer.cs
+++ PROJECT_PAD_2022/forms/ActionHRForm.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeader.Location = new System.Drawing.Point(13, 9);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(480, 35);
+            this.labelHeader.TabIndex = 0;
+            this.labelHeader.Text = "Add New Employee Form";
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ActionHRForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ActionHRForm";
+            this.ClientSize = new System.Drawing.Size(505, 450);
+            this.Controls.Add(this.labelHeader);
+            this.Name = "ActionHRForm";
+            this.Text = "Human Resource Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelHeader;
     }
 }
